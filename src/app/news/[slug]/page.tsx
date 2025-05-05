b2308f8209c26d01789be6829e3baa630fb186ba
anglerBite <jackall489@gmail.com>
2025-05-06 02:38:32 +0900

    6章まで完了

---
+++ src/app/news/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import Article from "@/app/_components/Article";
+import { getNewsDetail } from "@/app/_libs/microcms";
+import styles from './page.module.css';
+import ButtonLink from "@/app/_components/ButtonLink/ButtonLink";
+import { notFound } from "next/navigation";
+
+type Props = {
+    params: {
+        slug: string;
+    }
+};
+
+export default async function Page({ params }: Props) {
+    const { slug } = await params;
+    const data = await getNewsDetail(slug).catch(notFound);
+
+    return (
+        <>
+            <Article data={data} />
+            <div className={styles.footer}>
+                <ButtonLink href="/news">ニュース一覧へ</ButtonLink>
+            </div>
+        </>
+    )
+}
